0c8de67d924d03ec6ed7b140a9c730d6e4fcc8a2
Devin <devinpquinn4@gmail.com>
2025-08-17 15:51:09 -0400

    scene setup

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameController : MonoBehaviour
+{
+    public Animator swordAnim;
+    public TextMeshProUGUI attemptsTextBackground;
+    public TextMeshProUGUI attemptsTextForeground;
+    public Image backgroundImage;
+}
