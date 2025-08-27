3c80967f7e81941c74e25834cb9cac3da5fca357
Devin <devinpquinn4@gmail.com>
2025-08-27 19:30:06 -0400

    sfx implemented

--- Assets/Scripts/NameEntryManager.cs
+++ Assets/Scripts/NameEntryManager.cs
@@ -89,7 +89,7 @@ public class NameEntryManager : MonoBehaviour
     
     IEnumerator WaitAndDisable()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.75f);
         gameObject.SetActive(false);
     }
 }
